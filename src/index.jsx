d8b8cea6b07070940899d245b7db9aa66851e016
Edgar Varela <edgarvarela24@gmail.com>
2025-07-13 12:08:28 -0700

    feat: add routing

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,14 @@
 /* @refresh reload */
-import { render } from 'solid-js/web'
-import './index.css'
-import App from './App.jsx'
+import { render } from 'solid-js/web';
+import { Router } from '@solidjs/router';
+import App from './App';
+import './index.css';
 
-const root = document.getElementById('root')
-
-render(() => <App />, root)
+render(
+  () => (
+    <Router>
+      <App />
+    </Router>
+  ),
+  document.getElementById('root')
+);
